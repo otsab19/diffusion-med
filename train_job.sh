9b2a0b005f5dfba61ad482b459df8194f3c290f5
otsab <chaliseutsab@gmail.com>
2024-10-05 11:16:58 +0100

    changes for collab

--- train_job.sh
+++ train_job.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export PYTHONPATH="/home/cbtil/Documents/SRDIFF/DisC-Diff"
-mpiexec -n 2 python scripts/super_res_train.py --config config/config_train.yaml
+export PYTHONPATH="/content/DisC-Diff:$PYTHONPATH"  # Adjust PYTHONPATH to include the current directory
+
+python scripts/super_res_sample.py --config config/config_test.yaml
