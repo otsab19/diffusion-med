4d8c7183a816cbd9756e69aeb7a092948a1836ce
otsab <chaliseutsab@gmail.com>
2024-10-06 11:26:43 +0100

    changes for collab

--- train_job.sh
+++ train_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export PYTHONPATH="/home/cbtil/Documents/SRDIFF/DisC-Diff"
+export PYTHONPATH="/content/diffusion-med:$PYTHONPATH"
 mpiexec -n 2 python scripts/super_res_train.py --config config/config_train.yaml
