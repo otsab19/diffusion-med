450b2222491e1855739879e65510092d86848632
otsab <chaliseutsab@gmail.com>
2024-10-06 11:34:18 +0100

    changes for collab

--- train_job.sh
+++ train_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH="/content/diffusion-med:$PYTHONPATH"
-mpiexec -n 2 python scripts/super_res_train.py --config config/config_train.yaml
+mpiexec -n 1 python scripts/super_res_train.py --config config/config_train.yaml
