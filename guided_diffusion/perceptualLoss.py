a5f57816413b6256cd061f482c97aef5e930119c
otsab <chaliseutsab@gmail.com>
2024-10-21 22:33:54 +0100

    vgg16 for single channel

--- guided_diffusion/perceptualLoss.py
+++ guided_diffusion/perceptualLoss.py
@@ -3,28 +3,22 @@ import torch.nn as nn
 import torchvision.models as models
 
 class PerceptualLossVGG16(nn.Module):
-    def __init__(self, model_path=None, layers=None, use_l1=True):
+    def __init__(self, model_path=None, use_l1=True):
         super(PerceptualLossVGG16, self).__init__()
-        # Load the VGG model from a given path or use a pre-trained model
-        if model_path is not None:
-            vgg = models.vgg16()
-            vgg.load_state_dict(torch.load(model_path))  # Load model weights from the provided path
-            vgg = vgg.features  # We only need the feature layers
-        else:
-            vgg = models.vgg16(pretrained=True).features
+        vgg = models.vgg16(pretrained=False)  # Do not load pre-trained weights here
+        if model_path:
+            vgg.load_state_dict(torch.load(model_path), strict=False)  # Load weights with strict=False to allow for changes
 
-        # Freeze VGG16 parameters
-        for param in vgg.parameters():
-            param.requires_grad = False
+        # Modify the first convolutional layer to accept 1-channel grayscale input
+        old_weights = vgg.features[0].weight.data
+        new_weights = old_weights.mean(dim=1, keepdim=True)  # Average over RGB channels to get grayscale equivalent
+        vgg.features[0] = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        vgg.features[0].weight.data = new_weights
 
-        # Specify the layers to use for perceptual loss
-        if layers is None:
-            self.layers = [4, 9, 16, 23]  # conv1_2, conv2_2, conv3_3, conv4_3
-        else:
-            self.layers = layers
+        # Extract the required feature layers for perceptual loss
+        self.layers = [4, 9, 16, 23]  # Corresponding to conv1_2, conv2_2, conv3_3, conv4_3
+        self.vgg = nn.Sequential(*[vgg.features[i] for i in range(max(self.layers) + 1)])
 
-        # Create a sequential model with selected layers
-        self.vgg = nn.Sequential(*[vgg[i] for i in range(max(self.layers) + 1)])
         self.criterion = nn.L1Loss() if use_l1 else nn.MSELoss()
 
     def forward(self, x, target):
