dd3b43c8878d2389a38c48f7bb444275762ead31
otsab <chaliseutsab@gmail.com>
2024-10-23 11:45:00 +0100

    debug

--- guided_diffusion/unet.py
+++ guided_diffusion/unet.py
@@ -491,7 +491,7 @@ class UNetModel(nn.Module):
         self.num_head_channels = num_head_channels
         self.num_heads_upsample = num_heads_upsample
 
-        self.feedback_attention = FeedbackAttention(in_channels)
+        self.feedback_attention = FeedbackAttention(96)
         time_embed_dim = model_channels * 4
         self.time_embed = nn.Sequential(
             linear(model_channels, time_embed_dim),
@@ -713,8 +713,7 @@ class UNetModel(nn.Module):
         # Apply feedback from previous pass (if available)
         if feedback is not None:
             print("--", len(feedback), len(hs))
-            print(hs[0].shape)
-            hs = [self.feedback_attention(h, f) for h, f in zip(hs, feedback)]
+            print(hs[0].shape); hs = [self.feedback_attention(h, f) for h, f in zip(hs, feedback)]
             # hs = self.feedback_attention(hs, feedback)
 
         # Store feedback for the next forward pass
